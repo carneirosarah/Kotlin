e4d7cd27b516d8a326d61bbdd27d5559dbe4163c
Sarah <sarahrlcarneiro@gmail.com>
2020-10-02 15:46:48 -0300

    Adicao dos metodos consultarCurso, registrarCurso, excluirCurso, consultarProfessor, registrarProfessorAdjunto, registrarProfessorTitular, excluirProfessor, consultarAluno, matricularAluno e alocarProfessores a classe DigitalHouseManager.

--- src/br/digitalHouse/desafioKotin/Matricula.kt
+++ src/br/digitalHouse/desafioKotin/Matricula.kt
@@ -4,8 +4,7 @@ import java.util.*
 
 // Construtor da classe Matricula contendo aluno e curso.
 data class Matricula (val aluno: Aluno,
-                      val curso: Curso,
-                      val codigoMatricula: Int
+                      val curso: Curso
 ) {
 
     // O atributo dataMatricula e inicializado com a data do dia
