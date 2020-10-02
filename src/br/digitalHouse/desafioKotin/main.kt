fac0366a559e40c96084ad1469d0afac819b1e6f
Sarah <sarahrlcarneiro@gmail.com>
2020-10-02 16:39:30 -0300

    Implementacao da funcao main

--- src/br/digitalHouse/desafioKotin/main.kt
+++ src/br/digitalHouse/desafioKotin/main.kt
@@ -1,22 +1,40 @@
 package br.digitalHouse.desafioKotin
 
 fun main() {
-    val aluno = Aluno("Sarah", "Carneiro", 1)
-    println(aluno.equals(Aluno("Pedro", "Silva", 1)))
-    println(aluno.toString())
 
-    val professorTitular = ProfessorTitular("prof", "teste", 1, 1, "teste")
-    println(professorTitular.equals(ProfessorTitular("prof2", "teste2", 4, 1, "teste2")))
-    println(professorTitular.toString())
+    val digitalHouseManager = DigitalHouseManager(mutableMapOf<Int, Aluno>(), mutableMapOf<Int, Professor>(), mutableMapOf<Int, Curso>(), mutableMapOf<Int, Matricula>())
 
-    val professorAdjunto = ProfessorAdjunto("adj", "teste3", 5, 2, 8)
-    println(professorAdjunto.equals(ProfessorAdjunto("profadj", "teste4", 6, 2, 1)))
-    println(professorAdjunto.toString())
+    // registra dois professores titulares
+    digitalHouseManager.registrarProfessorTitular("Maria", "Silva", 1, "Kotlin")
+    digitalHouseManager.registrarProfessorTitular("Laura", "Santos", 2, "Java")
 
-    val curso = Curso("teste", 1, professorTitular, professorAdjunto, mutableMapOf<Int, Aluno>(), 10)
-    println(curso.equals(Curso("teste2", 1, professorTitular, professorAdjunto, mutableMapOf<Int, Aluno>(), 5)))
-    println(curso.toString())
+    // registra dois professores adjuntos
+    digitalHouseManager.registrarProfessorAdjunto("Carolina", "Souza", 3, 8)
+    digitalHouseManager.registrarProfessorAdjunto("Marcia", "Gomes", 4, 6)
 
-    val matricula = Matricula(aluno, curso)
-    println(matricula.toString())
+    // registra dois cursos
+    digitalHouseManager.registrarCurso("Full Stack", 20001, 3)
+    digitalHouseManager.registrarCurso("Android", 20002, 2)
+
+    // aloca um professor titular e um adjunto para cada curso
+    digitalHouseManager.alocarProfessores(20001, 2,4)
+    digitalHouseManager.alocarProfessores(20002, 1,3)
+
+    // registra cinco alunos
+    digitalHouseManager.matricularAluno("Pedro", "Passos", 1)
+    digitalHouseManager.matricularAluno("Aline", "Jardim", 2)
+    digitalHouseManager.matricularAluno("Mariana", "Monteiro", 3)
+    digitalHouseManager.matricularAluno("Mateus", "Lima", 4)
+    digitalHouseManager.matricularAluno("Fernanda", "Pacheco", 5)
+
+    // matricula dois alunos no curso de Full Stack
+    digitalHouseManager.matricularAluno(1, 20001)
+    digitalHouseManager.matricularAluno(2, 20001)
+
+    // matricula tres alunos no curso de Android
+    digitalHouseManager.matricularAluno(3, 20002)
+    digitalHouseManager.matricularAluno(4, 20002)
+    digitalHouseManager.matricularAluno(5, 20002)
+
+    println(digitalHouseManager.toString())
 }
